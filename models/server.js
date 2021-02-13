0c3d80bd726a0656d7d1e1b83dfb2650768f02d5
Diego Mendoza <diegoemg996@gmail.com>
2021-02-13 16:31:55 -0600

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios'
+
+        //Midlewares
+        this.midlewares();
+        //Rutas
+        this.routes();
+    }
+
+    midlewares() {
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(express.static('public'));
+    }
+
+
+    routes() {
+        this.app.use(this.usuariosPath, require('../routes/usuarios'));
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port)
+        })
+    }
+}
+
+
+module.exports = Server;
